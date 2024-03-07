1cbab98e0d7996627a3471760e9b03464a5280f1
Vlad <vlad@turn.lv>
2024-03-07 19:36:03 +0200

    adding plugin and char counter

--- packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
+++ packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
@@ -16,25 +16,44 @@ import './tinymce-webcomponent.js';
 @customElement(`${prefix}-text-editor`)
 class TextEditor extends LitElement {
   @property({ type: Boolean }) readonly = false;
+  @property({ type: String }) textEditorTitle = 'Vertejuma pamatojums';
+  @property({ type: String }) limit = '1000';
+  @property({ type: String }) editorId = 'editor';
+  @property({ type: Object }) editorConfig = {
+    height: 200,
+    highlight_on_focus: true,
+    promotion: false,
+    ajax_url: 'https://httpbin.org/post',
+    max_length: 1000,
+    external_plugins: {
+      pluginId:
+        'http://localhost:8001/plugins/background_color_change/plugin.min.js',
+    },
+  };
 
-  toggleReadonly() {
-    this.readonly = !this.readonly;
-  }
+  // toggleReadonly() {
+  //   this.readonly = !this.readonly;
+  // }
 
   render() {
-    return html`
-      <cds-button @click="${this.toggleReadonly}">Toggle Readonly</cds-button>
+    //@ts-ignore
+    window['config_' + this.editorId] = this.editorConfig;
 
+    console.log('editorConfig', this.editorConfig);
+    return html`
+      <p>${this.textEditorTitle}</p>
+      <p>
+        ${window['config_' + this.editorId + '.actual_length']}/${this.limit}
+      </p>
       <tinymce-editor
-        height="300"
+        id="${this.editorId}"
+        config="${'config_' + this.editorId}"
         menubar="false"
         ?readonly="${this.readonly}"
-        plugins="advlist autolink lists link image charmap print preview anchor 
-    searchreplace visualblocks code fullscreen 
-    insertdatetime media table paste code help wordcount"
         toolbar="undo redo | formatselect | bold italic backcolor | 
     alignleft aligncenter alignright alignjustify | 
     bullist numlist outdent indent | removeformat | help | link"
+        plugins="background_color_change advlist autosave save sender autolink lists link image charmap preview anchor pagebreak code visualchars wordcount"
         content_css="//www.tiny.cloud/css/codepen.min.css"
         promotion="false">
         &lt;p&gt;This recreates the same settings as the &lt;a
