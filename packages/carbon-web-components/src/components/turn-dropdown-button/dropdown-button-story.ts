3fff3fbef46bb1fce6e7e2ab8f281c11f48f3913
Vlad <vlad@turn.lv>
2024-03-21 10:23:11 +0200

    adding turn-dropdown-button component

---
+++ packages/carbon-web-components/src/components/turn-dropdown-button/dropdown-button-story.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2019, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+import './index';
+
+export const Default = () => {
+  const itemsArray = [
+    { text: 'Status 1', value: 'Status 1' },
+    { text: 'Status 2', value: 'Status 2' },
+    { text: 'Status 3', value: 'Status 3' },
+  ];
+  const eventHandler = (event) => {
+    console.log('itemSelected', event.detail.item.__value);
+  };
+  return html`
+    <cds-dropdown-button
+      .accordionItems=${itemsArray}
+      .dropdownHandler=${eventHandler}>
+    </cds-dropdown-button>
+  `;
+};
+
+export default {
+  title: 'Custom-components/Dropdown Button',
+};
