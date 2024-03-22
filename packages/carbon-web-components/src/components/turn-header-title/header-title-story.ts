688bf62612bdd2eb38e8aadb42c3cfedeb2ccf09
Vlad <vlad@turn.lv>
2024-03-22 13:15:04 +0200

    adding status control panel and changes in placings and typography

---
+++ packages/carbon-web-components/src/components/turn-header-title/header-title-story.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2019, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+import './index';
+import storyDocs from './header-title-story.mdx';
+
+export const Default = () => {
+  return html` <cds-header-title></cds-header-title> `;
+};
+export const Propsed = () => {
+  return html`
+    <cds-header-title title
+     </cds-header-title>
+  `;
+};
+
+export default {
+  parameters: {
+    ...storyDocs.parameters,
+  },
+  title: 'Custom-components/Header Title',
+};
