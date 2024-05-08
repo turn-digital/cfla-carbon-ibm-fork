4ea4d432c5b7ab7b5942e6dbb568ea20905533e3
Vlad <vlad@turn.lv>
2024-05-08 13:59:16 +0300

    adding func log in in js

--- packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
+++ packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
@@ -58,6 +58,8 @@ class TextEditor extends LitElement {
     window[configKey] = this.combinedEditorConfig;
     window[`${configKey}_invalidateQueriesFunc`] = this.invalidateQueriesFunc;
 
+    console.log('this.invalidateQueriesFunc', this.invalidateQueriesFunc);
+
     window.localStorage.setItem(
       `${this.editorId}_content`,
       this.textEditorData
