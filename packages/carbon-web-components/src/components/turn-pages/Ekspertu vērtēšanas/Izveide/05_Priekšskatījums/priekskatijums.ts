648a4d50a5ae9325667927666c9df92fb5453901
Vlad <vlad@turn.lv>
2024-05-31 21:26:18 +0300

    adding page 6 of experts

---
+++ packages/carbon-web-components/src/components/turn-pages/Ekspertu vērtēšanas/Izveide/05_Priekšskatījums/priekskatijums.ts
@@ -0,0 +1,191 @@
+import { LitElement, html } from 'lit';
+import { prefix } from '../../../../../globals/settings';
+import { carbonElement as customElement } from '../../../../../globals/decorators/carbon-element';
+
+@customElement(`${prefix}-ekspertu-priekskatijums`)
+class CDSPVertesanasEkspertuPriekskatijums extends LitElement {
+  render() {
+    const contextualHeaderApiData = {
+      virsraksts: 'Atlase: 1.1.1',
+      saturs: [
+        {
+          tips: 'status',
+          nosaukums: 'Atlases statuss',
+          links: {
+            nosaukums: null,
+            saite: null,
+          },
+          teksts: null,
+          statuss: {
+            nosaukums: 'Izsludināta',
+            krasa: 'red',
+            datums: null,
+          },
+        },
+        {
+          tips: 'link',
+          nosaukums: 'Nosaukums',
+          links: {
+            nosaukums:
+              'Pētniecības un inovāciju kapacitātes stiprināšana un progresīvu tehnoloģiju ieviešana',
+            saite: 'http://localhost:31475/LV/SAMPamatdati/View/4',
+          },
+          teksts: null,
+          statuss: {
+            nosaukums: null,
+            krasa: null,
+            datums: null,
+          },
+        },
+        {
+          tips: 'status',
+          nosaukums: 'Atlases statuss',
+          links: {
+            nosaukums: null,
+            saite: null,
+          },
+          teksts: null,
+          statuss: {
+            nosaukums: 'Izsludināta',
+            krasa: 'green',
+            datums: '12.12.2012',
+          },
+        },
+
+        {
+          tips: 'divider',
+          nosaukums: null,
+          links: {
+            nosaukums: null,
+            saite: null,
+          },
+          teksts: null,
+          statuss: {
+            nosaukums: null,
+            krasa: null,
+            datums: null,
+          },
+        },
+        {
+          tips: 'link',
+          nosaukums: 'Politikas programma',
+          links: {
+            nosaukums: '1.1',
+            saite:
+              'http://localhost:31475/LV/IeguldijumuPrioritatesPamatdati/View/3',
+          },
+          teksts: null,
+          statuss: {
+            nosaukums: null,
+            krasa: null,
+            datums: null,
+          },
+        },
+      ],
+    };
+
+    const chosenCategoriesData = [
+      { label: 'Projekta dzīves cikla posms', value: 'Sākumposma' },
+      {
+        label: 'Eksperta tips',
+        value: 'CFLA ekspertu vērtējumsar nodaļas vadītāja iesaisti',
+      },
+      { label: 'Ekspertīzes joma', value: 'Valsts atbalsta ' },
+      { label: 'Termiņš', value: '12.03.2024' },
+      { label: '*Eksperts', value: 'Jānis Bērziņš' },
+      {
+        label: 'Projekta iesniegumi',
+        value: `3.1.1.5/19/A/043
+      3.1.1.5/19/A/043
+      3.1.1.5/19/A/043
+      3.1.1.5/19/A/043`,
+      },
+      {
+        label: 'Vertēšanas veidlapas sagatave',
+        value: `<cds-link href="#">Sagatave.docx</cds-link>`,
+      },
+      {
+        label: 'Uzdevums ekspertam',
+        value: 'Veikt vērtēšanu atbilstoši noteikumiem',
+      },
+    ];
+    return html`
+      <link
+        rel="stylesheet"
+        href="https://demo.turn.lv/cfla_dist/assets/css/turn-carbon-grid.css" />
+      <cds-main-content-block storybook>
+        <cds-contextual-header-api
+          class="cds--css-grid-column cds--col-span-100"
+          .contextualHeaderApiData=${contextualHeaderApiData}>
+        </cds-contextual-header-api>
+        <cds-main-content-title
+          class="cds--css-grid-column cds--col-span-100 cds--grid-column-hang"
+          title="Ekspertu vērtējuma izveidošana">
+        </cds-main-content-title>
+        <div
+          class="cds--css-grid-column cds--col-span-100 cds--grid-column-hang">
+          <cds-progress-indicator space-equally>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Vērtēšanas veids"
+              state="complete"></cds-progress-step>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Termiņš"
+              state="complete"></cds-progress-step>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Ekspertu katalogs"
+              state="complete"></cds-progress-step>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Projektu saraksts"
+              state="complete"></cds-progress-step>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Dokumenti"
+              state="complete"></cds-progress-step>
+            <cds-progress-step
+              style="max-inline-size: none;"
+              label="Priekšskatījums"
+              state="current"></cds-progress-step>
+          </cds-progress-indicator>
+        </div>
+        <cds-divider size="8"></cds-divider>
+        <div class="cds--css-grid-column cds--col-span-100 ">
+          <cds-inline-notification
+            style="min-width: 100%; margin-bottom: .5rem"
+            title="Tiks izveidota vērtēšanas “sagatave”"
+            subtitle="Lai pabeigtu vērtēšanas izveidi, pievienotu vērtējamos dokumentus, ekspertus un ekspertu vērtēšanas termiņus jāiet katrā projektā atsevišķi"
+            kind="info"
+            low-contrast>
+          </cds-inline-notification>
+          <cds-divider size="5"></cds-divider>
+          <cds-inline-notification
+            style="min-width: 100%; margin-bottom: .5rem"
+            title="*Ja pievieno ekspertu* Vērtējumi tiks nodoti ekspertam"
+            subtitle="Izveidojot vērtējumus, tie nonāks statusā “nodots ekspertam”"
+            kind="info"
+            low-contrast>
+          </cds-inline-notification>
+        </div>
+        <div class="cds--css-grid-column cds--col-span-100 ">
+          <cds-divider size="3"></cds-divider>
+          <cds-chosen-categories .chosenCategoriesData=${chosenCategoriesData}>
+          </cds-chosen-categories>
+        </div>
+
+        <div
+          class="cds--css-grid-column cds--sm:col-span-4 cds--md:col-span-8 cds--lg:col-span-10 cds--lg:col-span-10">
+          <cds-divider size="5"></cds-divider>
+          <cds-button-set>
+            <cds-button href="https://www.ibm.com">Atpakaļ</cds-button>
+            <cds-button href="https://www.ibm.com">Izveidot</cds-button>
+          </cds-button-set>
+        </div>
+      </cds-main-content-block>
+    `;
+  }
+}
+
+export default CDSPVertesanasEkspertuPriekskatijums;
