d2c8276f968932a8b3f5abfdf2ca4d97206b96ef
Vlad <vlad@turn.lv>
2024-11-21 17:57:12 +0200

    text-editor v1.2

--- packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
+++ packages/carbon-web-components/src/components/turn-text-editor/text-editor.ts
@@ -45,7 +45,7 @@ class TextEditor extends LitElement {
       editorId: this.editorId,
       fullscreen_native: true,
       browser_spellcheck: true,
-      cache_suffix: '?v=1.1',
+      cache_suffix: '?v=1.2',
     };
 
     // Merge defaultConfig with this.editorConfig, prioritizing properties from this.editorConfig
