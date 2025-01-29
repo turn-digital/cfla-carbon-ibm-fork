a4c473a26c55d5605b21e09abf57a29aaf753b5c
Vladislavs <vladislavs@turn.lv>
2025-01-29 18:46:49 +0200

    add hp-darbibu-saraksts new layout

--- packages/carbon-web-components/src/components/turn-pages/HP admin/Atlases HP darbibu saraksts/00_darbibu-saraksts-tukss/darbibu-saraksts-tukss-story.ts
+++ packages/carbon-web-components/src/components/turn-pages/HP admin/Atlases HP darbibu saraksts/00_darbibu-saraksts-tukss/darbibu-saraksts-tukss-story.ts
@@ -36,104 +36,112 @@ const statusHistoryItemsArr = [
 
 export const Propsed = () => {
   return html`
-    <cds-main-content-block-open>
-      <div slot="header" class="cds--css-grid-column cds--col-span-100">
-        <cds-contextual-header title="Projekta iesniegums: 3.1.1.5/19">
+    <cds-doc-page>
+      <span slot="header">
+        <cds-contextual-header title="Atlase: 1.1.1.1">
           <cds-label-value-horizontal label="Atlases nosaukums:">
-            <cds-link href="#">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <cds-link href="#"
+              >Zinātnes politikas ieviešana, vadība un kapacitātes stiprināšana
             </cds-link>
           </cds-label-value-horizontal>
-          <cds-label-value-horizontal label="Atlases nosaukums:">
+          <cds-divider size="3"></cds-divider>
+          <cds-label-value-horizontal label="Atlases status">
             <cds-tag type="blue">Noslēgusies</cds-tag> 25.10.2019 10:34
           </cds-label-value-horizontal>
           <cds-divider size="3"></cds-divider>
-          <cds-label-value-horizontal label="Atlases nosaukums:">
+          <cds-label-value-horizontal label="Politikas programma">
             <cds-link href="#">1.1 </cds-link>
           </cds-label-value-horizontal>
         </cds-contextual-header>
-
-        <div class="cds--css-grid cds--css-grid--narrow">
+        <cds-divider size="5"></cds-divider>
+        <div class="cds--subgrid cds--subgrid--condensed">
           <div
-            class="cds--sm:col-span-4 cds--md:col-span-5 cds--lg:col-span-10 cds--css-grid-column">
-            <cds-main-content-item size="07">
-              <cds-header-title
-                title="Horizontālo principu konfigurēšana v1"></cds-header-title>
-            </cds-main-content-item>
-            <cds-main-content-item size="07">
-              <cds-status-history
-                .statusHistoryItems=${statusHistoryItemsArr}
-                statusHistoryTitle="Statusu vēsture"
-                currenStatusColor="grey"
-                currenStatusTitle="Sagatavošanā"
-                currenStatusDate="25.10.2019 10:34"></cds-status-history>
-            </cds-main-content-item>
+            class="cds--css-grid-column cds--sm:col-span-4 cds--md:col-span-5 cds--lg:col-span-10 cds--grid-column-hang">
+            <cds-header-title title="Horizontālo principu konfigurēšana v1">
+            </cds-header-title>
+            <cds-divider size="5"></cds-divider>
+            <cds-status-history
+              .statusHistoryItems="${statusHistoryItemsArr}"
+              statusHistoryTitle="Statusu vēsture"
+              currenStatusColor="grey"
+              currenStatusTitle="Sagatavošanā"
+              currenStatusDate="25.10.2019 10:34">
+            </cds-status-history>
+            <cds-divider size="5"></cds-divider>
           </div>
           <div
-            class="cds--sm:col-span-4 cds--md:col-span-5 cds--lg:col-span-6 cds--css-grid-column">
-            <div>
-              <cds-main-content-item size="07" isStatusChangePanel>
-                <cds-dropdown-button
-                  buttonLabel="Mainīt statusu"
-                  .statusArray="${statusArray}"></cds-dropdown-button>
-              </cds-main-content-item>
-            </div>
+            class="cds--css-grid-column cds--sm:col-span-4 cds--md:col-span-5 cds--lg:col-span-6">
+            <cds-main-content-item size="0" isStatusChangePanel>
+              <cds-dropdown-button
+                buttonLabel="Mainīt statusu"
+                .statusArray="${statusArray}"></cds-dropdown-button>
+            </cds-main-content-item>
+
+            <cds-divider size="5"></cds-divider>
           </div>
         </div>
-      </div>
-      <div slot="content" class="cds--css-grid-column cds--col-span-100">
-        <div class="cds--css-grid-column cds--col-span-100">
-          <cds-print-download
-            downloadLink="https://example.com/download"
-            printLink="https://example.com/print">
-          </cds-print-download>
-          <cds-divider></cds-divider>
-
-          <cds-table size="sm">
-            <cds-table-header-title slot="title"
-              >Atlases HP darbību saraksts</cds-table-header-title
-            >
-            <cds-table-toolbar slot="toolbar">
-              <cds-table-toolbar-content>
-                <cds-button
-                  kind="secondary"
-                  tooltip-alignment=""
-                  tooltip-position="top"
-                  type="button"
-                  id="modal-example-button"
-                  has-main-content="">
-                  Pievienot ${Add16({ slot: 'icon' })}
-                </cds-button>
-              </cds-table-toolbar-content>
-            </cds-table-toolbar>
-            <cds-table-head>
-              <cds-table-header-row>
-                <cds-table-header-cell>HP darbību grupa</cds-table-header-cell>
-                <cds-table-header-cell>Ietekmes veids</cds-table-header-cell>
-                <cds-table-header-cell>Min. HP rad. sk.</cds-table-header-cell>
-                <cds-table-header-cell>HP rad. sk.</cds-table-header-cell>
-                <cds-table-header-cell>Min. HP darb. sk.</cds-table-header-cell>
-                <cds-table-header-cell>HP darb. sk.</cds-table-header-cell>
-              </cds-table-header-row>
-            </cds-table-head>
-            <cds-table-body> </cds-table-body>
-          </cds-table>
-          <cds-empty-state
-            text="Šobrīd nav veikti līguma grozījumi"
-            linkUrl="#"
-            linkText="Pievienot">
-          </cds-empty-state>
-          <cds-pagination page-size="100" total-items="100">
-            <cds-select-item value="10">10</cds-select-item>
-            <cds-select-item value="20">20</cds-select-item>
-            <cds-select-item value="30">30</cds-select-item>
-            <cds-select-item value="40">40</cds-select-item>
-            <cds-select-item value="50">50</cds-select-item>
-            <cds-select-item value="100">100</cds-select-item>
-          </cds-pagination>
-        </div>
-      </div>
-    </cds-main-content-block-open>
+      </span>
+      <span slot="content">
+        <cds-doc-page-tab-content withPrintDownload>
+          <span slot="tab-print-download">
+            <cds-print-download
+              downloadLink="https://example.com/download"
+              printLink="https://example.com/print">
+            </cds-print-download
+          ></span>
+          <span slot="tab-content"
+            ><cds-table size="sm">
+              <cds-table-header-title slot="title"
+                >Atlases HP darbību saraksts</cds-table-header-title
+              >
+              <cds-table-toolbar slot="toolbar">
+                <cds-table-toolbar-content>
+                  <cds-button
+                    kind="secondary"
+                    tooltip-alignment=""
+                    tooltip-position="top"
+                    type="button"
+                    id="modal-example-button"
+                    has-main-content="">
+                    Pievienot ${Add16({ slot: 'icon' })}
+                  </cds-button>
+                </cds-table-toolbar-content>
+              </cds-table-toolbar>
+              <cds-table-head>
+                <cds-table-header-row>
+                  <cds-table-header-cell
+                    >HP darbību grupa</cds-table-header-cell
+                  >
+                  <cds-table-header-cell>Ietekmes veids</cds-table-header-cell>
+                  <cds-table-header-cell
+                    >Min. HP rad. sk.</cds-table-header-cell
+                  >
+                  <cds-table-header-cell>HP rad. sk.</cds-table-header-cell>
+                  <cds-table-header-cell
+                    >Min. HP darb. sk.</cds-table-header-cell
+                  >
+                  <cds-table-header-cell>HP darb. sk.</cds-table-header-cell>
+                </cds-table-header-row>
+              </cds-table-head>
+              <cds-table-body> </cds-table-body>
+            </cds-table>
+            <cds-empty-state
+              text="Šobrīd nav veikti līguma grozījumi"
+              linkUrl="#"
+              linkText="Pievienot">
+            </cds-empty-state>
+            <cds-pagination page-size="100" total-items="100">
+              <cds-select-item value="10">10</cds-select-item>
+              <cds-select-item value="20">20</cds-select-item>
+              <cds-select-item value="30">30</cds-select-item>
+              <cds-select-item value="40">40</cds-select-item>
+              <cds-select-item value="50">50</cds-select-item>
+              <cds-select-item value="100">100</cds-select-item>
+            </cds-pagination></span
+          >
+        </cds-doc-page-tab-content>
+      </span>
+    </cds-doc-page>
   `;
 };
 
